11839bf5a85413e386a6e0b64abe092b555c1911
VL1507 <vova.mashyanov@gmail.com>
2026-02-24 18:23:26 +1000

    add methods for check exists user by name or email

--- backend/src/mood_tracker/domain/repositories/user_repository.py
+++ backend/src/mood_tracker/domain/repositories/user_repository.py
@@ -2,9 +2,13 @@ from abc import abstractmethod
 from typing import Protocol
 
 from mood_tracker.domain.entities import User
+from mood_tracker.domain.value_objects import UserEmail, UserName
 
 
 class IUserRepository(Protocol):
     @abstractmethod
-    def save(self, user: User) -> None:
-        pass
+    def save(self, user: User) -> None: ...
+    @abstractmethod
+    def exists_by_name(self, name: UserName) -> bool: ...
+    @abstractmethod
+    def exists_by_email(self, email: UserEmail) -> bool: ...
