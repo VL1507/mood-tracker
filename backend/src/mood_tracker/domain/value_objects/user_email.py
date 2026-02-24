23e67bd5bb904b2e1585703b430df2c0a8be6b39
VL1507 <vova.mashyanov@gmail.com>
2026-02-24 21:26:05 +1000

    add checking the correctness of email

--- backend/src/mood_tracker/domain/value_objects/user_email.py
+++ backend/src/mood_tracker/domain/value_objects/user_email.py
@@ -1,6 +1,14 @@
+import re
 from dataclasses import dataclass
 
 
 @dataclass(slots=True, frozen=True)
 class UserEmail:
     value: str
+
+    def __post_init__(self) -> None:
+        # TODO: найти хороший паттерн
+        pattern = r"[^@]+@[^@]+\.[^@]+"
+        if not re.match(pattern, self.value):
+            # TODO: заменить ошибку
+            raise ValueError
