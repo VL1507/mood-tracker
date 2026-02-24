4aa42eca717037eee93169ea7355b87619b4ae81
VL1507 <vova.mashyanov@gmail.com>
2026-02-24 21:03:54 +1000

    add service for jwt

---
+++ backend/src/mood_tracker/domain/security/token_service.py
@@ -0,0 +1,17 @@
+from abc import abstractmethod
+from typing import Protocol
+
+from mood_tracker.domain.value_objects import TokenPair, UserID
+
+
+class ITokenService(Protocol):
+    @abstractmethod
+    def generate_token_pair(self, user_id: UserID) -> TokenPair: ...
+    @abstractmethod
+    def verify_access(self, access_token: str) -> bool: ...
+    @abstractmethod
+    def revoke_access(self, access_token: str) -> None: ...
+    @abstractmethod
+    def verify_refresh(self, user_id: UserID, refresh_token: str) -> bool: ...
+    @abstractmethod
+    def revoke_refresh(self, user_id: UserID, refresh_token: str) -> None: ...
