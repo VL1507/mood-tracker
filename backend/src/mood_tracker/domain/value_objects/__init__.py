66949aa0a772ae635695328f56d17c53fd36c9ba
VL1507 <vova.mashyanov@gmail.com>
2026-02-24 17:29:30 +1000

    add value objects to improve imports

---
+++ backend/src/mood_tracker/domain/value_objects/__init__.py
@@ -0,0 +1,6 @@
+from .hash_password import HashPassword
+from .user_email import UserEmail
+from .user_id import UserID
+from .user_name import UserName
+
+__all__ = ["HashPassword", "UserEmail", "UserID", "UserName"]
