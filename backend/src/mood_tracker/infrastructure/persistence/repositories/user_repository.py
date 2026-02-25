2dfd2d662b332f0645724c7ecda047a582fe192b
VL1507 <vova.mashyanov@gmail.com>
2026-02-25 22:05:10 +1000

    Add an implementation of IUserRepository to SQLAlchemy

---
+++ backend/src/mood_tracker/infrastructure/persistence/repositories/user_repository.py
@@ -0,0 +1,38 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from mood_tracker.domain.entities import User
+from mood_tracker.domain.repositories import IUserRepository
+from mood_tracker.domain.value_objects import HashPassword, UserEmail, UserID
+from mood_tracker.infrastructure.persistence.models import UserORM
+
+
+class SQLAlchemyUserRepository(IUserRepository):
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
+
+    async def save(self, user: User) -> None:
+        user_orm = self._domain_to_orm(user_domain=user)
+        self.session.add(user_orm)
+        await self.session.commit()
+
+    async def exists_by_email(self, email: UserEmail) -> bool:
+        stmt = select(UserORM).where(UserORM.email == email.value)
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none() is not None
+
+    @staticmethod
+    def _domain_to_orm(user_domain: User) -> UserORM:
+        user_orm = UserORM()
+        user_orm.id = user_domain.id.value
+        user_orm.email = user_domain.email.value
+        user_orm.hash_password = user_domain.hash_password.value
+        return user_orm
+
+    @staticmethod
+    def _orm_to_domain(user_orm: UserORM) -> User:
+        return User(
+            id=UserID(user_orm.id),
+            email=UserEmail(user_orm.email),
+            hash_password=HashPassword(user_orm.hash_password),
+        )
