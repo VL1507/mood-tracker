62908a56167e86a16e443e0e056180d9afbe54c6
VL1507 <vova.mashyanov@gmail.com>
2026-02-27 23:54:27 +1000

    add TokenRepository to provider

--- backend/src/mood_tracker/presentation/dependencies/providers/token_provider.py
+++ backend/src/mood_tracker/presentation/dependencies/providers/token_provider.py
@@ -2,10 +2,11 @@ from dishka import Provider, Scope, provide
 from redis.asyncio import Redis
 
 from mood_tracker.config import Config
+from mood_tracker.domain.repositories import ITokenRepository
+from mood_tracker.infrastructure.security import RedisTokenRepository
 
 
 class TokenProvider(Provider):
-
     @provide(scope=Scope.APP)
     @staticmethod
     def get_redis(config: Config) -> Redis:
@@ -14,3 +15,8 @@ class TokenProvider(Provider):
             port=config.REDIS.PORT,
             password=config.REDIS.PASSWORD,
         )
+
+    @provide(scope=Scope.REQUEST)
+    @staticmethod
+    def get_token_repository(redis: Redis) -> ITokenRepository:
+        return RedisTokenRepository(redis=redis)
