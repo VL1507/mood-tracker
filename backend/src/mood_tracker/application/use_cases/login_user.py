8f089e4f69b9d8c4c0fe1189dfd491398c267c51
VL1507 <vova.mashyanov@gmail.com>
2026-02-28 22:28:14 +1000

    add use case for login

---
+++ backend/src/mood_tracker/application/use_cases/login_user.py
@@ -0,0 +1,32 @@
+from mood_tracker.domain.repositories import IUserRepository
+from mood_tracker.domain.security import IPasswordHasher, ITokenService
+from mood_tracker.domain.value_objects import (
+    TokenPair,
+    UserEmail,
+)
+
+
+class LoginUserUseCase:
+    def __init__(
+        self,
+        user_repo: IUserRepository,
+        password_hasher: IPasswordHasher,
+        token_service: ITokenService,
+    ) -> None:
+        self.user_repo = user_repo
+        self.password_hasher = password_hasher
+        self.token_service = token_service
+
+    async def __call__(self, email: str, password: str) -> TokenPair:
+        user = await self.user_repo.get_by_email(email=UserEmail(email))
+        if user is None:
+            # TODO: заменить ошибку
+            raise ValueError("user not exist")
+
+        if not self.password_hasher.verify_password(
+            plain_password=password, hashed_password=user.hash_password.value
+        ):
+            # TODO: заменить ошибку
+            raise ValueError("pwd")
+
+        return await self.token_service.generate_token_pair(user_id=user.id)
