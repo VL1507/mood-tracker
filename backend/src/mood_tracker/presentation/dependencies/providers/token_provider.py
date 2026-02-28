46f610960ed98e7022b7f88aa75073cd0f659cb2
VL1507 <vova.mashyanov@gmail.com>
2026-02-28 22:55:10 +1000

    edit types to redis

--- backend/src/mood_tracker/presentation/dependencies/providers/token_provider.py
+++ backend/src/mood_tracker/presentation/dependencies/providers/token_provider.py
@@ -13,7 +13,7 @@ from mood_tracker.infrastructure.security import (
 class TokenProvider(Provider):
     @provide(scope=Scope.APP)
     @staticmethod
-    def get_redis(config: Config) -> Redis:
+    def get_redis(config: Config) -> Redis[str]:
         return Redis(
             host=config.REDIS.HOST,
             port=config.REDIS.PORT,
@@ -23,7 +23,7 @@ class TokenProvider(Provider):
 
     @provide(scope=Scope.REQUEST)
     @staticmethod
-    def get_token_repository(redis: Redis) -> ITokenRepository:
+    def get_token_repository(redis: Redis[str]) -> ITokenRepository:
         return RedisTokenRepository(redis=redis)
 
     @provide(scope=Scope.REQUEST)
