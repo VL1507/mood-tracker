f3622d20b8f48be27ce7c61be3c2a08d9b75edd3
VL1507 <vova.mashyanov@gmail.com>
2026-02-28 22:29:23 +1000

    provide LoginUserUseCase

--- backend/src/mood_tracker/presentation/dependencies/providers/auth_use_cases.py
+++ backend/src/mood_tracker/presentation/dependencies/providers/auth_use_cases.py
@@ -1,6 +1,9 @@
 from dishka import Provider, Scope, provide
 
-from mood_tracker.application.use_cases import RegisterUserUseCase
+from mood_tracker.application.use_cases import (
+    LoginUserUseCase,
+    RegisterUserUseCase,
+)
 from mood_tracker.domain.repositories.user_repository import IUserRepository
 from mood_tracker.domain.security.password_hasher import IPasswordHasher
 from mood_tracker.domain.security.token_service import ITokenService
@@ -15,6 +18,19 @@ class AuthUseCasesProvider(Provider):
         token_service: ITokenService,
     ) -> RegisterUserUseCase:
         return RegisterUserUseCase(
+            user_repo=user_repo,
+            password_hasher=password_hasher,
+            token_service=token_service,
+        )
+
+    @provide(scope=Scope.REQUEST)
+    @staticmethod
+    def get_login_user_use_case(
+        user_repo: IUserRepository,
+        password_hasher: IPasswordHasher,
+        token_service: ITokenService,
+    ) -> LoginUserUseCase:
+        return LoginUserUseCase(
             user_repo=user_repo,
             password_hasher=password_hasher,
             token_service=token_service,
