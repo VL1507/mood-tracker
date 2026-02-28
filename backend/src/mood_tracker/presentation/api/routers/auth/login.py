49825f39935e63fed862d9b01966d7a644d2b3e6
VL1507 <vova.mashyanov@gmail.com>
2026-02-28 22:31:03 +1000

    add endpoint for login

---
+++ backend/src/mood_tracker/presentation/api/routers/auth/login.py
@@ -0,0 +1,33 @@
+from dishka.integrations.fastapi import FromDishka, inject
+from fastapi import APIRouter, Response
+
+from mood_tracker.application.use_cases import LoginUserUseCase
+from mood_tracker.config import Config
+from mood_tracker.presentation.api.schemas.auth import (
+    UserLoginRequest,
+    UserLoginResponse,
+)
+
+router = APIRouter()
+
+
+@router.post("/login")
+@inject
+async def login(
+    response: Response,
+    data: UserLoginRequest,
+    use_case: FromDishka[LoginUserUseCase],
+    config: FromDishka[Config],
+) -> UserLoginResponse:
+    token_pair = await use_case(email=data.email, password=data.password)
+
+    response.set_cookie(
+        key="refresh_token",
+        value=token_pair.refresh,
+        max_age=config.JWT.REFRESH_EXPIRE_SECONDS,
+        secure=False,  # TODO: в проде заменить на True
+        httponly=True,
+        samesite="lax",  # TODO: возможно стоит изменить
+    )
+
+    return UserLoginResponse(access_token=token_pair.access)
