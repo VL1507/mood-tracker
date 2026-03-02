e603650c8df9a0aaf937d7c41375614d04bb9a4b
VL1507 <vova.mashyanov@gmail.com>
2026-03-02 23:31:57 +1000

    add logout use case

--- backend/src/mood_tracker/application/use_cases/__init__.py
+++ backend/src/mood_tracker/application/use_cases/__init__.py
@@ -1,4 +1,5 @@
 from .login_user import LoginUserUseCase
+from .logout_user import LogoutUserUseCase
 from .register_user import RegisterUserUseCase
 
-__all__ = ["LoginUserUseCase", "RegisterUserUseCase"]
+__all__ = ["LoginUserUseCase", "LogoutUserUseCase", "RegisterUserUseCase"]
---
+++ backend/src/mood_tracker/application/use_cases/logout_user.py
@@ -0,0 +1,9 @@
+from mood_tracker.domain.security import ITokenService
+
+
+class LogoutUserUseCase:
+    def __init__(self, token_service: ITokenService) -> None:
+        self.token_service = token_service
+
+    async def __call__(self, refresh_token: str) -> None:
+        await self.token_service.revoke_refresh(refresh_token=refresh_token)
