53d2b7d07a0691327b2790c86a354502fcf72b3f
VL1507 <vova.mashyanov@gmail.com>
2026-03-03 00:12:53 +1000

    register refresh endpoint

--- backend/src/mood_tracker/presentation/api/routers/auth/setup.py
+++ backend/src/mood_tracker/presentation/api/routers/auth/setup.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter
 
-from . import login, register
+from . import login, logout, refresh, register
 
 
 def setup_routers(app: APIRouter) -> None:
     router = APIRouter(prefix="/auth", tags=["Auth"])
 
     router.include_router(login.router)
+    router.include_router(logout.router)
+    router.include_router(refresh.router)
     router.include_router(register.router)
 
     app.include_router(router)
