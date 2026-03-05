4fc6d9b4c871e8fd68eff4fdd082864a9ea85b5f
VL1507 <vova.mashyanov@gmail.com>
2026-03-06 00:52:21 +1000

    add dbprovider to container

--- backend/src/mood_tracker/presentation/dependencies/container.py
+++ backend/src/mood_tracker/presentation/dependencies/container.py
@@ -3,12 +3,13 @@ from dishka.integrations.fastapi import FastapiProvider
 
 from mood_tracker.config import Config
 
-from .providers import CookieServiceProvider
+from .providers import CookieServiceProvider, DBProvider
 
 
 def make_container_di(config: Config) -> AsyncContainer:
     return make_async_container(
         CookieServiceProvider(),
+        DBProvider(),
         FastapiProvider(),
         context={Config: config},
     )
