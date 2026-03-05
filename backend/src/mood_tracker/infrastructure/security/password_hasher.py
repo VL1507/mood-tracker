bc8e28f97055419c567823139d769b6db8bdfaf0
VL1507 <vova.mashyanov@gmail.com>
2026-03-06 00:57:45 +1000

    add PasswordHasher implementation of IPasswordHasher

---
+++ backend/src/mood_tracker/infrastructure/security/password_hasher.py
@@ -0,0 +1,19 @@
+from argon2 import PasswordHasher as Argon2PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+
+from mood_tracker.domain.security import IPasswordHasher
+
+
+class PasswordHasher(IPasswordHasher):
+    def __init__(self) -> None:
+        self._ph = Argon2PasswordHasher()
+
+    def hash_password(self, password: str) -> str:
+        return self._ph.hash(password=password)
+
+    def verify_password(self, password: str, password_hash: str) -> bool:
+        try:
+            self._ph.verify(hash=password_hash, password=password)
+        except VerifyMismatchError:
+            return False
+        return True
