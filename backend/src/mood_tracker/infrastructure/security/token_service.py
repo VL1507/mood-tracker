087da20edede95a27da8b9ab7c68b38979d5950d
VL1507 <vova.mashyanov@gmail.com>
2026-03-06 01:06:07 +1000

    add TokenService implementation of ITokenService

---
+++ backend/src/mood_tracker/infrastructure/security/token_service.py
@@ -0,0 +1,43 @@
+from datetime import UTC, datetime, timedelta
+from uuid import uuid4
+
+import jwt
+
+from mood_tracker.config import JWT
+from mood_tracker.domain.repositories import ITokenRepository
+from mood_tracker.domain.security import ITokenService
+from mood_tracker.domain.value_objects import TokenPair, UserID
+
+
+class TokenService(ITokenService):
+    def __init__(
+        self, token_repository: ITokenRepository, jwt_config: JWT
+    ) -> None:
+        self._token_repository = token_repository
+        self._algorithm = jwt_config.ALGORITHM
+        self._secret_key = jwt_config.SECRET_KEY
+        self._access_exp = jwt_config.ACCESS_EXPIRE_SECONDS
+        self._refresh_exp = jwt_config.REFRESH_EXPIRE_SECONDS
+
+    async def create_token_pair(self, user_id: UserID) -> TokenPair:
+        now = datetime.now(UTC)
+        payload = {
+            "sub": str(user_id.value),
+            "iat": now,
+            "exp": now + timedelta(seconds=self._access_exp),
+        }
+        access_token = jwt.encode(
+            payload=payload,
+            key=self._secret_key,
+            algorithm=self._algorithm,
+        )
+
+        refresh_token = str(uuid4())
+
+        await self._token_repository.save_refresh(
+            user_id=user_id,
+            refresh_token=refresh_token,
+            time_seconds=self._refresh_exp,
+        )
+
+        return TokenPair(access=access_token, refresh=refresh_token)
