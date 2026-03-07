b0d63d115257d127a399fed8a1300aa93b27a2e1
VL1507 <vova.mashyanov@gmail.com>
2026-03-08 01:50:14 +1000

    add router for refresh

---
+++ backend/src/mood_tracker/presentation/api/routers/auth/refresh.py
@@ -0,0 +1,36 @@
+from dishka.integrations.fastapi import FromDishka, inject
+from fastapi import APIRouter, Cookie, HTTPException, Response, status
+
+from mood_tracker.application.dto.refresh_user import RefreshUserInputDTO
+from mood_tracker.application.use_cases import RefreshUserUseCase
+from mood_tracker.constants import REFRESH_TOKEN_COOKIE_NAME
+from mood_tracker.presentation.api.cookie_service import CookieService
+from mood_tracker.presentation.api.schemas.auth import UserRefreshResponse
+
+router = APIRouter()
+
+
+@router.post(
+    "/refresh",
+    status_code=status.HTTP_200_OK,
+)
+@inject
+async def refresh(
+    response: Response,
+    use_case: FromDishka[RefreshUserUseCase],
+    cookie_service: FromDishka[CookieService],
+    refresh_token: str | None = Cookie(None, alias=REFRESH_TOKEN_COOKIE_NAME),
+) -> UserRefreshResponse:
+    if not refresh_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Refresh token missing",
+        )
+
+    output_dto = await use_case(
+        input_dto=RefreshUserInputDTO(refresh_token=refresh_token)
+    )
+    cookie_service.set_refresh_token(
+        response=response, token=output_dto.refresh_token
+    )
+    return UserRefreshResponse(access_token=output_dto.access_token)
