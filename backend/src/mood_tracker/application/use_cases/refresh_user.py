fc71cf85a8ac608f17b406061771ecf293018f03
VL1507 <vova.mashyanov@gmail.com>
2026-03-08 01:46:14 +1000

    add refresh use case

---
+++ backend/src/mood_tracker/application/use_cases/refresh_user.py
@@ -0,0 +1,32 @@
+from mood_tracker.application.dto.refresh_user import (
+    RefreshUserInputDTO,
+    RefreshUserOutputDTO,
+)
+from mood_tracker.application.exceptions import InvalidRefreshTokenError
+from mood_tracker.domain.security import ITokenService
+
+
+class RefreshUserUseCase:
+    def __init__(self, token_service: ITokenService) -> None:
+        self._token_service = token_service
+
+    async def __call__(
+        self, input_dto: RefreshUserInputDTO
+    ) -> RefreshUserOutputDTO:
+        user_id = await self._token_service.get_user_id_by_refresh(
+            refresh_token=input_dto.refresh_token
+        )
+        if user_id is None:
+            raise InvalidRefreshTokenError
+
+        await self._token_service.revoke_refresh(
+            refresh_token=input_dto.refresh_token
+        )
+
+        token_pair = await self._token_service.create_token_pair(
+            user_id=user_id
+        )
+
+        return RefreshUserOutputDTO(
+            access_token=token_pair.access, refresh_token=token_pair.refresh
+        )
