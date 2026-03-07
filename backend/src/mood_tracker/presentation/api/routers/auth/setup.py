9a5e5585888ddbf514b2a421e5bdb01e343c6260
VL1507 <vova.mashyanov@gmail.com>
2026-03-08 00:30:21 +1000

    include login router

--- backend/src/mood_tracker/presentation/api/routers/auth/setup.py
+++ backend/src/mood_tracker/presentation/api/routers/auth/setup.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 
-from . import register
+from . import login, register
 
 
 def setup_routers(api_router: APIRouter) -> None:
     router = APIRouter(prefix="/auth", tags=["Auth"])
 
+    router.include_router(login.router)
     router.include_router(register.router)
 
     api_router.include_router(router)
