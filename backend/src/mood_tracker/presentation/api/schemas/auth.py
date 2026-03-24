78ea12d33e7c338852f8251ec9f1cdd023d3b91c
VL1507 <vova.mashyanov@gmail.com>
2026-03-25 02:19:15 +1000

    add constant

--- backend/src/mood_tracker/presentation/api/schemas/auth.py
+++ backend/src/mood_tracker/presentation/api/schemas/auth.py
@@ -2,15 +2,17 @@ import re
 
 from pydantic import BaseModel, EmailStr, field_validator
 
+MIN_PASSWORD_LEN = 12
+
 
 class UserRegisterRequest(BaseModel):
     email: EmailStr
     password: str
 
-    @field_validator("password")  # TODO: возможно стоит переделать ошибки
+    @field_validator("password")
     @classmethod
     def validate_password(cls, password: str) -> str:
-        if len(password) < 12:
+        if len(password) < MIN_PASSWORD_LEN:
             raise ValueError("Password must be at least 12 characters long")
         if not any(c.isupper() for c in password):
             raise ValueError(
@@ -24,7 +26,8 @@ class UserRegisterRequest(BaseModel):
             raise ValueError("Password must contain at least one digit")
         if not re.search(r"[!@#$%^&*]", password):
             raise ValueError(
-                "Password must contain at least one special character (!@#$%^&*)"
+                "Password must contain at least"
+                " one special character (!@#$%^&*)"
             )
         return password
 
